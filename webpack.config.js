2bf13c92db3847684b76fdef17740bf9da62293a
Lucas Poirier <lpoiri01@students.cs.ubc.ca>
2024-04-13 21:36:23 -0700

    npm check 2

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    entry: './main.js', // Your main script file
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+    devServer: {
+        contentBase: './dist',
+    },
+};
