161c39388ac77b4c51be099b850cd0f9259188bb
Lucas Poirier <lpoiri01@students.cs.ubc.ca>
2024-04-13 20:41:42 -0700

    cube again 2

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 import * as THREE from './three.module.js'; // Adjust the path if necessary
-import { STLLoader } from './STLLoader.js'; // Adjust the path if necessary
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
