fa1ed17c754eeabcc06449b654382fef60f03c57
Lucas Poirier <lpoiri01@students.cs.ubc.ca>
2024-04-13 19:44:39 -0700

    three.js sphere 5

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import * as THREE from './assets/js/three.module.js';  // Path adjusted for the assets folder
-import { STLLoader } from './assets/js/STLLoader.js';  // Path adjusted for the assets folder
+import * as THREE from '../assets/js/three.module.js';  // Path adjusted for the assets folder
+import { STLLoader } from '../assets/js/STLLoader.js';  // Path adjusted for the assets folder
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
